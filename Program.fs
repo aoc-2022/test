5227c5df211986735fba2421ae7dadb261bc2117
xeno <xeno@lv427.space>
2022-11-24 18:31:11 +0100

    solved day 2

--- Program.fs
+++ Program.fs
@@ -1,23 +1,37 @@
-﻿open System.IO
+﻿open System
+open System.IO
 
-let lines = File.ReadAllLines "/Users/xeno/projects/aoc/test/input1.txt"
-let ints = lines |> Array.map int |> Array.toList 
+let lines = File.ReadAllLines "/Users/xeno/projects/aoc/test/input2.txt"
 
-// ints |> List.map (printfn "%d")
+let lineToDelta (line:string) =
+    let words = line.Split [|' '|] |> Array.toList
+    match words with
+    | ["forward";x] -> (x |> int,0)
+    | ["up";x] -> (0, - (x |> int))
+    | ["down";x] -> (0, x |> int)
+    | _ -> raise (Exception($"Invalid input {words}"))
 
-let rec check1 list =
-    match list with
-    | a::b::tail -> (if a < b then 1 else 0) + check1 (b::tail)
-    | [a] -> 0
-    | [] -> 0
-    
-let rec check2 list =
-    match list with
-    | a::b::c::d::tail -> (if (a+b+c) < (b+c+d) then 1 else 0) + check2 (b::c::d::tail)
-    | _ -> 0
- 
-let increases = check1 ints
-let increases2 = check2 ints
+let deltas = lines |> Array.map lineToDelta
 
-printfn "%d" increases
-printfn "%d" increases2
+let horizontal = deltas |> Array.map fst |> Array.sum
+let verticals = deltas |> Array.map snd |> Array.sum
+
+printfn $"(%d{horizontal},%d{verticals} => {horizontal * verticals})"
+
+// part 2
+
+let rec move (aim : int) ((x,y) : int*int) (lines : string list) : int*int = 
+    match lines with
+    | [] -> (x,y)
+    | line :: rest ->
+        match line.Split [|' '|] |> Array.toList with
+        | ["up";d] -> move (aim - (d |> int)) (x,y) rest
+        | ["down";d] -> move (aim + (d |> int)) (x,y) rest
+        | ["forward";d] -> move (aim) (x+(d |> int),y+(aim*(d |> int))) rest
+        | _ -> raise (Exception($"Unknown line: {line}"))
+
+let pos = move 0 (0,0) (lines |> Array.toList)
+printfn $"pos {pos} {fst pos * snd pos}"
+        
+        
+        
