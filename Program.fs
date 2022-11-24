58b2cd09b8b3f27f9d118dea28f997437f55cd96
xeno <xeno@lv427.space>
2022-11-24 17:58:13 +0100

    solved 2021 day 1

--- Program.fs
+++ Program.fs
@@ -1,2 +1,23 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System.IO
+
+let lines = File.ReadAllLines "/Users/xeno/projects/aoc/test/input1.txt"
+let ints = lines |> Array.map int |> Array.toList 
+
+// ints |> List.map (printfn "%d")
+
+let rec check1 list =
+    match list with
+    | a::b::tail -> (if a < b then 1 else 0) + check1 (b::tail)
+    | [a] -> 0
+    | [] -> 0
+    
+let rec check2 list =
+    match list with
+    | a::b::c::d::tail -> (if (a+b+c) < (b+c+d) then 1 else 0) + check2 (b::c::d::tail)
+    | _ -> 0
+ 
+let increases = check1 ints
+let increases2 = check2 ints
+
+printfn "%d" increases
+printfn "%d" increases2
