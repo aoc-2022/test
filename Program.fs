108b749554c98a8a7b67a73064c261588e599e1a
xeno <xeno@lv427.space>
2022-11-24 21:59:16 +0100

    day 4

--- Program.fs
+++ Program.fs
@@ -1,107 +1,93 @@
 ﻿open System
 open System.IO
 
-let lines = File.ReadAllLines "/Users/xeno/projects/aoc/test/input3.txt"
-let bits = lines
-           |> Array.toList
-           |> List.map Seq.toList
-           |> List.map (List.map (fun i -> if i = '0' then -1 else 1))
-           // |> List.map (printfn "%A")
-           
-           
-let rec addlist a b =
-    match (a,b) with
-    | (a::ar,b::br) -> a+b :: addlist ar br
-    | ([],[]) -> []
-    | _ -> raise (Exception($"List mismatch {(a,b)}"))
+let lines = File.ReadAllLines "/Users/xeno/projects/aoc/test/input4.txt" |> Array.toList 
 
-let bits2 = List.reduce addlist bits
+let splitOn list value =
+    let rec split acc list =
+        match list with
+        | [] -> [acc]
+        | v :: rest when v = value -> (List.rev acc) :: split [] rest
+        | v :: rest -> split (v::acc) rest
+    let res = split [] list
+    res 
 
-bits2 |> List.map (printfn "%A")
-printfn $"{bits2}"
-
-let rec topos bits =
-    match bits with
-    | a::rest -> (if a > 0 then 1 else 0) :: topos rest
-    | [] -> []
+let v = splitOn lines ""
+printfn $"{v}"    
     
-let rec toneg bits =
-    match bits with
-    | a::rest -> (if a < 0 then 1 else 0) :: toneg rest
-    | [] -> []
+let numbers = v |> List.head
+let boards = v |> List.tail
 
-let gamma_bits = topos bits2
-let epsilon_bits = toneg bits2
-
-printfn $"gamma: {gamma_bits} epsilon: {epsilon_bits}"
-
-let toDeci bits =
-    let bits = List.rev bits    
-    let rec toDeci bits =
-        match bits with
-        | [] -> 0
-        | [last] -> last
-        | a::b::rest -> a + 2*(toDeci (b::rest))
-    toDeci bits 
+type Board(numbers: int list, rows: Set<int> list, called: int) =
+    member this.Numbers = numbers
+    member this.Called = called
     
-let gamma = toDeci gamma_bits
-let epsilon = toDeci epsilon_bits
+    member this.isBingo : Boolean = rows |> List.contains Set.empty
+    
+    member this.call (num:int) =
+        let numbers = numbers |> List.filter (fun n -> n <> num)
+        let rows = rows |> List.map (Set.remove num)
+        // let called = called |> Set.add num
+        Board(numbers, rows, num)
+        
+    member this.Score =
+        let unused = numbers |> List.sum
+        unused * called 
+    
+    override this.ToString () =
+        $"Board({numbers} {rows}) called:{called}"
+    
+    new(board:int list list) =
+        let numbers = board |> List.concat
+        let rec toCols rows =
+            if List.head rows = [] then []
+            else
+                let col = rows |> List.map List.head
+                let rems = rows |> List.map List.tail
+                col :: toCols rems
+        let cols = toCols board
+        let lines = [cols;board] |> List.concat
+        let lines = lines |> List.map Set.ofList
+        Board(numbers,lines,0)
 
-printfn $"gamma: {gamma} eps: {epsilon} mult = {gamma*epsilon}"
+let toInts (s:string) =
+    s.Split [|' '|]
+    |> Array.filter (fun s -> s = "" |> not) 
+    |> Array.map int |> Array.toList
+let boards1 = boards |> List.map (List.map toInts)
+let boards2 = boards1 |> List.map Board
 
-let x = toDeci 
-           
-// part 2
+printfn $"{boards2}"
+let numbers2 = numbers |> List.head |> (fun s -> s.Split [|','|]) |> Array.map int |> Array.toList  
+printfn $"numbers: {numbers2}"
 
-let rec findOxygen (bitss : int list list) =
-    printfn $"findOxygen: {bitss |> List.length} {bitss}"
-    if bitss |> List.length = 1
-    then bitss |> List.head 
+let rec callUntilBingo (numbers: int list) (boards: Board list) : Board =
+    let number = numbers |> List.head 
+    if boards |> List.exists (fun board -> board.isBingo)
+    then boards |> List.filter (fun board -> board.isBingo) |> List.head
     else
-        if bitss |> List.head |> List.length = 0 then []
-        else 
-            let first = bitss
-                        |> List.map List.head
-                        |> List.sum
-            let value = if first >= 0 then 1 else -1
-            let rems = bitss
-                       |> List.filter (fun bl -> (List.head bl) = value)
-                       |> List.map List.tail
-            value :: findOxygen rems
+        let boards = boards |> List.map (fun board -> board.call number)
+        let numbers = numbers |> List.tail
+        callUntilBingo numbers boards 
 
-let rec findScrubber (bitss : int list list) =
-    printfn $"findOxygen: {bitss |> List.length} {bitss}"
-    if bitss |> List.length = 1
-    then bitss |> List.head 
-    else
-        if bitss |> List.head |> List.length = 0 then []
-        else 
-            let first = bitss
-                        |> List.map List.head
-                        |> List.sum
-            let value = if first < 0 then 1 else -1
-            let rems = bitss
-                       |> List.filter (fun bl -> (List.head bl) = value)
-                       |> List.map List.tail
-            value :: findScrubber rems
-            
-let oxygen = findOxygen bits
-             |> List.map (fun b -> if b = -1 then 0 else 1)
-             |> toDeci 
-printfn $"Oxygen: {findOxygen bits} {oxygen}"      
-            
-let scrubber = findScrubber bits
-             |> List.map (fun b -> if b = -1 then 0 else 1)
-             |> toDeci 
-printfn $"Scrubber: {findScrubber bits} {scrubber}"
+let bingo = callUntilBingo numbers2 boards2
 
-printfn $"res: {oxygen * scrubber}"
-   
-    
-    
-    
-    
-                
+printfn $"{bingo} {bingo.Numbers |> List.sum} {bingo.Called} {bingo.Score}"
+
+let rec callUntilLoss (numbers: int list) (boards: Board list) : Board =
+    printfn $"Call until loss: {numbers} {boards}"
+    let number = numbers |> List.head
+    let boards = boards |> List.map (fun board -> board.call number)
+    let boards2 = boards |> List.filter (fun board -> board.isBingo |> not)
+    if boards2 |> List.length = 0
+    then boards |> List.head 
+    else callUntilLoss (numbers |> List.tail) boards2 
+
+let notBingo = callUntilLoss numbers2 boards2
+
+printfn $"{notBingo} {notBingo.Numbers |> List.sum} {notBingo.Called} {notBingo.Score}"
+
+
     
            
            
